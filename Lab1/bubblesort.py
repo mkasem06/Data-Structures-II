87c5d8ba1307181b49a419b04e8be4d9093a8a63
eyad-amir <amireyad93@gmail.com>
2026-02-26 05:24:25 -0800

    selection

--- Lab1/bubblesort.py
+++ Lab1/bubblesort.py
@@ -12,8 +12,15 @@ def bubbleSort(arr):
                 swapped = True
         if swapped == False:
             break
-
-
+# Selection Sort
+def selectionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range (i+1 , n):
+            if arr[j] < arr[min_idx]:
+              min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
 elements = int(input("Enter number of elements: "))
 
